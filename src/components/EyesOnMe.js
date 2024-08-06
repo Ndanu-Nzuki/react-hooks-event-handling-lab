46b4c213dc04fdc8ab554a81a4613cfcae9c0e2a
Laurine Ndanu Nzuki <nzukilaurine@gmail.com>
2024-08-06 22:07:17 +0300

    Handles events in React

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+// components/EyesOnMe.js
+import React from 'react';
+
+const EyesOnMe = function() {
+  const handleFocus = function() {
+    console.log('Good!');
+  };
+
+  const handleBlur = function() {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
+
